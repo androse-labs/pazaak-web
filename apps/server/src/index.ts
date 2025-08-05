8cf01c0d469ecc81d5029ba17581877560fcdea5
Braith Anderson <braithanderson1@gmail.com>
2025-08-05 18:37:53 +1000

    add web domain to web server

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -19,7 +19,7 @@ export const createApp = (
   app.use(
     '*',
     cors({
-      origin: ['http://localhost:5173'],
+      origin: ['http://localhost:5173', 'https://pazaak.androse.dev'],
     }),
   )
 
